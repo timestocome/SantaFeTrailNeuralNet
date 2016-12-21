d107794536317307ff3ebf84d10cf36664f1fcf3
Linda MacPhee-Cobb <timestocome@gmail.com>
2016-12-21 11:42:50 -0600

    Wrote functions to create the maze data for the deep training Q program

---
+++ CreateData_DeepQ.py
@@ -0,0 +1,174 @@
+# http://github.com/timestocome
+
+# Attempt to use Deep Q Learning on SantaFe Ant problem
+# this version uses a neural network. 
+# In the Atari verison they use 3 convolutional layers
+# and two fully connected layers
+# A much simpler version should work here.
+
+
+import numpy as np
+import pandas as pd
+import sys
+import random
+import copy
+
+
+
+##############################################################################
+# init
+##############################################################################
+# set up game board
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+
+maze = [
+1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+]
+
+
+# constants
+number_of_actions = 4       # possible moves at each state
+epsilon = 0.10              # random moves
+number_of_game_positions = len(maze)
+learning_rate = 0.50
+total_reward = 0
+
+# create random action models for each state
+models = []
+for i in range(number_of_game_positions):
+    r = np.random.random(4) 
+    models.append(r/sum(r))
+
+
+################################################################################
+# read in data created below
+#################################################################################
+
+#input = pd.read_csv("training_data1.csv")
+
+
+
+###############################################################################
+# update game functions
+###############################################################################
+
+
+# return random (epsilon) action
+def random_action():
+    return random.randint(0, 3)
+
+
+
+###############################################################################
+# create data for training neural network
+###############################################################################
+# We need copies of the game board array
+# those will get fed into the convolutional network
+# Save the reward as a one hot vector mapping the reward onto the action prediction
+# Need a mark to show player location, 2 for now
+# the Atari group used 4 shots, 2 should work for this problem
+# x: 1024, y: 4
+
+
+training_data = np.zeros(1024+4)
+
+# reset maze ??? times
+for g in range(20):        # number of games to use for training
+
+# random walk through maze ??? times
+    state = -1
+    new_state = -1
+    reward = -1
+    for i in range(512):        # amount of wandering per game
+
+        # initalize game board
+        if i == 0:
+            reward_maze = copy.copy(maze)
+            state = 0
+            new_state = 0
+            reward = 1
+            reward_maze[0] = 2        # mark player location
+
+        # take random action
+        action = random_action()
+
+        if action == 0:         # up
+                if state > 31:  new_state = state - 32
+        elif action == 1:       # right
+                if ((state + 1) % 32 != 0 and state < 1023):  new_state = state + 1 
+        elif action == 2:       # down 
+                if state < (1023-31):  new_state = state + 32
+        elif action == 3:       # left
+                if (state % 32 != 0 and state > 0): new_state = state - 1
+        
+        if new_state == None: new_state = state     # impossible move
+        reward = reward_maze[new_state]             # see if food pellet still there
+
+
+        # map reward, if any to action vector ( 4 zeros, 1 in the action we took if there is a reward there )
+        action_rewards = np.zeros(number_of_actions)
+        if new_state != state:      # make sure we moved so we don't count player mark as a reward
+            action_rewards[action] = reward
+
+
+        # save current maze as input reward and vector as output target 
+        data = np.concatenate((reward_maze, action_rewards))
+
+        # add to stack
+        training_data = np.vstack((training_data, data))
+
+        # make new maze current maze
+        reward_maze[state] = 0          # remove reward and or player mark
+        reward_maze[new_state] = 2      # mark updated player location
+
+# delete dummy row we needed for vstack
+training_data = np.delete(training_data, 0, axis=0)
+
+
+print("saving data")
+np.savetxt("training_data.csv", training_data)
+print("Saved data: rows, cols", len(training_data), training_data.shape)
+
+
+print("reading data")
+# test can read in file
+input = pd.read_csv("training_data.csv")
+print("read in training data", len(input))
+#print(training_vectors.head())
+
+
