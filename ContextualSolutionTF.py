302cc6e2ec072d99446bf98fc311103d45bc7b6b
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-02-14 16:34:37 -0600

    Added contextual reinforcement solution
    See Simple Reinforcement Learning w/ TF 1.5
    https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-1-5-contextual-bandits-bff01d1aad9c#.bkwbz442r

---
+++ ContextualSolutionTF.py
@@ -0,0 +1,159 @@
+
+# http://github.com/timestocome
+
+
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+
+
+
+
+
+
+# the SantaFe Ant Trail Maze
+new_maze = np.asarray([
+        1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+], dtype='float32')
+
+
+# create maze
+class contextual_maze():
+
+    def __init__(self):
+
+        self.state = 0
+        self.states = new_maze
+        self.num_states = len(new_maze)
+        self.num_actions = 4
+
+    
+
+    def getState(self):
+        return self.state
+
+
+    def move(self, action):
+
+        state = self.state
+
+         # take a step if we can with out falling off edge of our world
+        if action == 0:         # up
+            if state > 31:                                  self.state = state - 32
+        elif action == 1:       # right
+            if ((state + 1) % 32 != 0 and state < 1023):    self.state = state + 1 
+        elif action == 2:       # down 
+            if state < (1023-31):                           self.state = state + 32
+        elif action == 3:       # left
+            if (state % 32 != 0 and state > 0):             self.state = state - 1
+        
+
+
+        result = self.states[self.state]  # collect reward
+        self.states[self.state] = 0        # remove reward from maze
+
+        if result == 1: return 1 
+        else: return 0
+
+
+
+class agent():
+
+    def __init__(self, lr, s_size, a_size):
+
+        self.state_in = tf.placeholder(shape=[1], dtype=tf.int32)
+        state_in_OH = slim.one_hot_encoding(self.state_in, s_size)
+        output = slim.fully_connected(state_in_OH, 
+                                        a_size, 
+                                        biases_initializer=None, 
+                                        activation_fn=tf.nn.sigmoid,
+                                        weights_initializer=tf.ones_initializer())
+        self.output = tf.reshape(output, [-1])
+        self.chosen_action = tf.argmax(self.output, 0)
+        self.reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
+        self.responsible_weight = tf.slice(self.output, self.action_holder, [1])
+        self.loss = -(tf.log(self.responsible_weight) * self.reward_holder)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+        self.update = optimizer.minimize(self.loss)
+
+
+
+tf.reset_default_graph()
+
+cMaze = contextual_maze()
+myAgent = agent(lr=0.001, s_size = cMaze.num_states, a_size = cMaze.num_actions)
+
+weights = tf.trainable_variables()[0]
+total_episodes = 10000
+total_reward = np.zeros([cMaze.num_states, cMaze.num_actions])
+e = 0.1     # random action 
+
+init = tf.global_variables_initializer()
+
+
+with tf.Session() as sess:
+    sess.run(init)
+    i = 0
+
+    while i < total_episodes:
+        s = cMaze.getState()
+
+        if np.random.rand(1) < e:
+            action = np.random.randint(cMaze.num_actions)
+        else:
+            action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in:[s]})
+
+        reward = cMaze.move(action)
+        
+        feed_dict = { myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]  }
+        
+        _, ww = sess.run([myAgent.update, weights], feed_dict = feed_dict)
+
+        total_reward[s, action] += reward
+
+        if i % 500 == 0:
+            print ("Mean reward for each of the " + str(cMaze.num_states) + " bandits: " + str(np.mean(total_reward, axis=1)))
+        
+        i += 1
+
+
+for a in range(cMaze.num_states):
+    print("The agent thinks action " + str(np.argmax(ww[a]) + 1) + " for state " + str(a+1) + " is the most promising ")
+
+    if np.argmax(ww[a]) == np.argmin(cMaze.states[a]):
+        print ("... and it was right")
+    else:
+        print( "and it was wrong ")
